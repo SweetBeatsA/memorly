745cb637aef703b032b95bf21ca755badc012465
DongGunYoon <donggun961108@gmail.com>
2023-02-19 20:16:11 +0900

    Upt. update the format of forms folder

--- server/forms/user.go
+++ server/forms/user.go
@@ -1,17 +1,10 @@
 package forms
 
-type UserForm struct{}
-
 type RegisterForm struct {
 	Email    string `json:"email" binding:"required"`
 	Name     string `json:"name" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
-
-// type UpdateForm struct {
-// 	Email string `json:"email" binding:"required"`
-// 	Name  string `json:"name" binding:"required"`
-// }
 
 type LoginForm struct {
 	Email    string `json:"email" binding:"required"`
