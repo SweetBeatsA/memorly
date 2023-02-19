dc411d260d75faa694ef06432861e0e5a2e70b0c
DongGunYoon <donggun961108@gmail.com>
2023-02-19 20:16:51 +0900

    Upt. update the models folder to only containing the entity

--- server/models/user.go
+++ server/models/user.go
@@ -1,51 +1,12 @@
 package models
 
 import (
-	"errors"
-	"gin/forms"
-
-	"golang.org/x/crypto/bcrypt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type UserModel struct{}
-
 type User struct {
-	Id       int    `json:"id" gorm:"primary_key"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-var users = []User{
-	{Id: 1, Name: "First User", Email: "first@gmail.com", Password: "firstPassword"},
-	{Id: 2, Name: "Second User", Email: "second@gmail.com", Password: "secondPassword"},
-	{Id: 3, Name: "Third User", Email: "third@gmail.com", Password: "thirdPassword"},
-}
-
-// var authModel = new(AuthModel)
-
-// func login(form forms.LoginForm) (token authModel.Token, err error) {
-// 	var token authModel.Token
-
-// 	return token, nil
-// }
-
-func register(form forms.RegisterForm) (user User, err error) {
-	bytePassword := []byte(form.Password)
-	hashedPassword, err := bcrypt.GenerateFromPassword(bytePassword, bcrypt.DefaultCost)
-
-	if err != nil {
-		return user, errors.New("Please check the password format")
-	}
-
-	user.Id = len(users) + 1
-	user.Name = form.Name
-	user.Email = form.Email
-	user.Password = string(hashedPassword)
-
-	return user, err
-}
-
-func getAll() (users []User, err error) {
-	return users, err
+	Id       primitive.ObjectID `json:"id,omitempty"`
+	Name     string             `json:"name,omitempty"`
+	Email    string             `json:"email,omitempty"`
+	Password string             `json:"password,omitempty"`
 }
