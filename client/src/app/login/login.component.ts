606708f32ca436ba2841b0d57a971c42cf466ac3
aplan42 <alexharvey125@gmail.com>
2023-02-20 14:38:45 -0500

    Alternate login with show/hide password

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+
+
+
 @Component({ templateUrl: 'login.component.html',
 styleUrls: ["./login.component.css"] })
-export class LoginComponent {}
+export class LoginComponent {
+    public showPassword: boolean = false;
+
+    public togglePasswordVisibility(): void {
+        this.showPassword = !this.showPassword;
+      }
+}
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,4 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({ templateUrl: 'register.component.html',
 styleUrls: ["./register.component.css"] })
-export class RegisterComponent {}
+export class RegisterComponent {
+
+    public showPassword: boolean = false;
+
+    public togglePasswordVisibility(): void {
+        this.showPassword = !this.showPassword;
+      }
+}
