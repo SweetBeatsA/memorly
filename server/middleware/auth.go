e2898fbf6cb3360db9d8fdb271e2d0bd20fd2271
DongGunYoon <donggun961108@gmail.com>
2023-02-20 22:38:47 +0900

    Add. add the middleware to check the JWT Token

---
+++ server/middleware/auth.go
@@ -0,0 +1,31 @@
+package middleware
+
+import (
+	"fmt"
+	"memorly/helpers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("Authorization")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization header provided")})
+			c.Abort()
+			return
+		}
+
+		signature, err := helpers.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+		c.Set("email", signature.Email)
+		c.Set("name", signature.Name)
+		c.Set("id", signature.Id)
+		c.Next()
+	}
+}
