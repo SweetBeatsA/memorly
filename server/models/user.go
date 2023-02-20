dd809e33f3ba2ace09a1d4ecd3912883f4bef7ff
DongGunYoon <donggun961108@gmail.com>
2023-02-20 22:32:39 +0900

    Upt. update the User model to include the created_at and updated_at values

--- server/models/user.go
+++ server/models/user.go
@@ -1,12 +1,16 @@
 package models
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	Id       primitive.ObjectID `json:"id,omitempty"`
-	Name     string             `json:"name,omitempty"`
-	Email    string             `json:"email,omitempty"`
-	Password []byte             `json:"password,omitempty"`
+	Id         primitive.ObjectID `json:"id,omitempty"`
+	Name       string             `json:"name,omitempty"`
+	Email      string             `json:"email,omitempty"`
+	Password   string             `json:"password,omitempty"`
+	Created_at time.Time          `json:"created_at`
+	Updated_at time.Time          `json:"updated_at`
 }
