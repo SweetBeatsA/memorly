09f634a0d7b6f3fb33544839fd0dc8fe88158421
aplan42 <alexharvey125@gmail.com>
2023-02-27 16:32:59 -0500

    Update

--- client/src/app/stepper/stepper.component.ts
+++ client/src/app/stepper/stepper.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 
 @Component({
   selector: 'app-stepper',
-  templateUrl: './stepper.component.html',
-  styleUrls: ['./stepper.component.css']
+  template: `
+    <div>
+      <button data-cy="decrement" (click)="decrement()">-</button>
+      <span data-cy="counter">{{ count }}</span>
+      <button data-cy="increment" (click)="increment()">+</button>
+    </div>
+  `,
 })
 export class StepperComponent {
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  @Input() count = 0
+  @Output() change = new EventEmitter()
+
+  increment(): void {
+    this.count++
+    this.change.emit(this.count)
+  }
+
+  decrement(): void {
+    this.count--
+    this.change.emit(this.count)
+  }
 }
