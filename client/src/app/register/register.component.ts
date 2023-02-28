2b82036c9afca0c62a0be843b59714dabf5ab029
aplan42 <alexharvey125@gmail.com>
2023-02-28 13:02:44 -0500

    Trying to implement FE login with axios

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({ templateUrl: 'register.component.html',
+
+//selector: 'app-root',
+//  template: 
+//    <button (click)="signUp()">Sign Up</button>,
+
 styleUrls: ["./register.component.css"] })
+
+
 export class RegisterComponent {
 
     public showPassword: boolean = false;
@@ -9,4 +17,33 @@ export class RegisterComponent {
     public togglePasswordVisibility(): void {
         this.showPassword = !this.showPassword;
       }
+
+      public signUp(): void {
+        const data = {
+          email: 'tester@gmail.com',
+          password: 'testerPassword',
+          name: 'tester'
+        };
+    
+        axios.post('http://api.memorly.kro.kr/users/signup', data)
+          .then(response => console.log(response))
+          .catch(error => console.error(error));
+      }
+/*
+      axios.post('api.memorly.kro.kr/users/signup', {
+        email: 'tester@gmail.com',
+        password: 'testerPassword',
+        name: 'tester'
+      }, {
+        headers: {
+          Authorization: 'your_token_here'
+        }
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+      */
 }
