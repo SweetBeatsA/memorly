ea4458bc2caa6fbedc69ca01c3d5b5eef4db4ed5
DongGunYoon <donggun961108@gmail.com>
2023-03-01 03:35:49 +0900

    Upt. update the cors setting from the server

--- server/main.go
+++ server/main.go
@@ -4,12 +4,14 @@ import (
 	"memorly/configs"
 	"memorly/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func setupRouter() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
+	router.Use(cors.Default())
 
 	configs.ConnectDB()
 
