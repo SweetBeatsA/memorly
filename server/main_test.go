13d24a2e63af2a85d8d70ebae5147cfb7cd29d81
RodolfoDA2201 <73867617+RodolfoDA2201@users.noreply.github.com>
2023-03-01 00:13:07 -0500

    unit test for sign up api (not complete)
     (not complete)

--- server/main_test.go
+++ server/main_test.go
@@ -1,28 +1,32 @@
 package main
 
 import (
+    "bytes"
+    "encoding/json"
+    "io/ioutil"
     "net/http"
     "net/http/httptest"
     "testing"
     "github.com/gin-gonic/gin"
+    "github.com/rs/xid"
     "github.com/stretchr/testify/assert"
 )
 
-func SetUpRouter() *gin.Engine{
-    router := gin.Default()
+func SetUpRouter() *gin.Engine {
+	router := gin.Default()
 
-	router.GET("/getuser", func(c *gin.Context){
+	router.GET("/user", func(c *gin.Context) {
 		c.String(http.StatusOK, "successful")
 	})
 
-    return router
-};
+	return router
+}
 
-func TestGetUser(t *testing.T){
+func TestGetUser(t *testing.T) {
 	router := setupRouter()
 
 	//request to /login
-	request, err := http.NewRequest("GET", "/getuser", nil)
+	request, err := http.NewRequest("GET", "/user", nil)
 	assert.NoError(t, err)
 
 	//record response
@@ -34,5 +38,35 @@ func TestGetUser(t *testing.T){
 
 	//check that response from body is "successful"
 	assert.Equal(t, "successful", w.Body.String())
+}
+
+func TestSignUp(t *testing.T){
+	//creating the router
+	router := gin.Default()
+
+	//signup endpoint
+	router.POST("/signup", func(c *gin.Context){
+
+		var requestBody map[string]string
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		//checking if email and password are present
+		if requestBody["email"] == "" || requestBody["password"] == "" {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "email and password are required"})
+			return
+		}
+
+		user := struct{
+			Username string 'json:"username"'
+			Password string 'json:"password"'
+		}{
+			Username: requestBody["username"],
+			Password: requestBody["password"]
+		}
+
+	})
 
 }
