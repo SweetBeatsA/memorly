954c224f2e89eba81b13d7c56ea8f2b0da8d4054
aplan42 <alexharvey125@gmail.com>
2023-03-01 17:05:38 -0500

    Tring to implement login

---
+++ client/src/app/_services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'http://api.memorly.kro.kr/users/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'login', {
+      username,
+      password
+    }, httpOptions);
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'signup', {
+      username,
+      email,
+      password
+    }, httpOptions);
+  }
+}
+
+//Note: Some code is found from https://www.bezkoder.com/angular-12-jwt-auth/ tutorial on using JWT tokens and authenticating login requests.
