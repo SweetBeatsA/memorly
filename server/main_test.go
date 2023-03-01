b339dbeccee6a77f8a7a1c5820a359263de55a32
RodolfoDA2201 <73867617+RodolfoDA2201@users.noreply.github.com>
2023-02-28 20:14:20 -0500

    created main_test.go
    made unit test for log in api

---
+++ server/main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+    "github.com/gin-gonic/gin"
+    "github.com/stretchr/testify/assert"
+)
+
+func SetUpRouter() *gin.Engine{
+    router := gin.Default()
+
+	router.GET("/login", func(c *gin.Context){
+		c.String(http.StatusOK, "successful")
+	})
+
+    return router
+};
+
+func TestLogIn(t *testing.T){
+	router := setupRouter()
+
+	//request to /login
+	request, err := http.NewRequest("GET", "/login", nil)
+	assert.NoError(t, err)
+
+	//record response
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, request)
+
+	//checking if response 200 OK is ok
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	//check that response from body is "successful"
+	assert.Equal(t, "successful", w.Body.String())
+
+}
