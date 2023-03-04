139c3da7cef5f3859720389b938da2f2b5396169
DongGunYoon <donggun961108@gmail.com>
2023-03-04 18:32:09 +0900

    Upt. update test case for sign up with duplicate email case

--- server/main_test.go
+++ server/main_test.go
@@ -42,6 +42,35 @@ func TestSignUp(t *testing.T) {
 	assert.NotNil(t, resp.Data)
 }
 
+func TestSignUpWithDuplicateEmail(t *testing.T) {
+	type Response struct {
+		Status  int                    `json:"status"`
+		Message string                 `json:"message"`
+		Data    map[string]interface{} `json:"data"`
+	}
+
+	router := setupRouter()
+
+	email := "tester@gmail.com"
+	data := map[string]string{"email": email, "password": "testerPassword", "name": "test"}
+	body, _ := json.Marshal(data)
+
+	request, err := http.NewRequest("POST", "/users/signup", bytes.NewBuffer(body))
+	assert.NoError(t, err)
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, request)
+
+	var resp Response
+	err = json.Unmarshal(w.Body.Bytes(), &resp)
+
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, "Email Already Taken", resp.Message)
+	assert.Equal(t, 400, resp.Status)
+	assert.Nil(t, resp.Data)
+}
+
 func TestLogIn(t *testing.T) {
 	type Response struct {
 		Status  int                    `json:"status"`
