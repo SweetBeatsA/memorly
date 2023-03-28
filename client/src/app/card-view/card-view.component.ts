e1732f49fe68e6b5372157011c09386071b0bcb8
aplan42 <alexharvey125@gmail.com>
2023-03-27 20:57:01 -0400

    Adding card view

--- client/src/app/card-view/card-view.component.ts
+++ client/src/app/card-view/card-view.component.ts
@@ -1,7 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Router } from '@angular/router';
 
 
 @Component({ templateUrl: 'card-view.component.html',
 styleUrls: ["./card-view.component.css"] })
 
-export class CardViewComponent {}
+export class CardViewComponent {
+
+    position = 0;
+    frontItems: string[] = [''];
+    backItems: string[] = [''];
+    cards: string[][] = [[''], ['']];
+    size = this.frontItems.length;
+    showFront: boolean = true;
+
+    constructor(private http: HttpClient, private router:Router) { }
+
+    ngOnInit(): void {
+        this.http.get('http://api.memorly.kro.kr/cards', { "headers": {"Authorization": sessionStorage.getItem('accessToken') || ""} }).subscribe((response: any) => {
+          this.cards = response.data.cards.map((card: any) => ({
+            //id: folder.id,
+            //title: folder.title,
+            //icon: folder.type === 'document' ? 'description' : 'folder'
+          }));
+        });
+      }
+
+    cycleLeft() {
+        if (this.position != 0) {
+            this.position--;
+        }
+        else {
+            this.position = this.size - 1;
+        }
+      }
+    
+      cycleRight() {
+        if (this.position != this.size - 1) {
+            this.position++;
+        }
+        else {
+            this.position = 0;
+        }
+      }
+
+      flipCard() {
+        if (this.showFront) {
+            this.showFront = false;
+        }
+        else {
+            this.showFront = true;
+        }
+      }
+
+}
